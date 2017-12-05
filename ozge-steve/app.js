43bed7aa6287778f35278ce99823a97e59c625b0
Steven Carpenter <stevegcarpenter@gmail.com>
2017-12-05 13:25:29 -0800

    Removed unnecessary console.log messages

--- ozge-steve/app.js
+++ ozge-steve/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-  console.log('jQuery ready!');
   $('.icon-menu').click(function(){
-    console.log('Detected click!')
     $('.main-nav').slideToggle('slow');
   });
 });
